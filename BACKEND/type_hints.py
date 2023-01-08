59e36ad2b6d9430c4f31bdaad5d9f74c629a98d4
Alvaro Gonzalez <alvarogds04@outlook.com>
2023-01-08 20:24:33 +0100

    1er Dia pyhton

--- BACKEND/type_hints.py
+++ BACKEND/type_hints.py
@@ -1,7 +1,19 @@
-
-
-
-##hello word
+##Type hints
 
 my_string_variable = "My string variable"
+
+
 print(my_string_variable)
+print(type(my_string_variable))
+
+
+my_string_variable= 5
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_typed_variable: str= 5
+my_typed_variable
+my_typed_variable = 5
+print(type(my_typed_variable))
+
+
